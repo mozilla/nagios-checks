2f873e1e031cc9559b03f1ae5fe435a0e3610e22
Michal Purzynski <michal@mozilla.com>
2017-04-21 16:47:13 -0700

    A cross-Ubuntu version of the check, will also work on RHEL

--- check_auditd.sh
+++ check_auditd.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-STATUS=$(/sbin/auditctl -s)
-TOK=($STATUS)
+# Sample $STATUS output on Ubuntu 14:
+# AUDIT_STATUS: enabled=1 flag=1 pid=7187 rate_limit=1000 backlog_limit=32768 lost=1430139 backlog=7
+# Sample $STATUS output on Ubuntu 16:
+# enabled 1
+# failure 1
+# pid 1604
+# rate_limit 1000
+# backlog_limit 32768
+# lost 11652
+# backlog 0
+# backlog_wait_time 15000
+# loginuid_immutable 0 unlocked
+
+PATH="/sbin:/bin:/usr/sbin:/usr/bin"
 EX=0
-PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-#Sample $STATUS output:
-#AUDIT_STATUS: enabled=1 flag=0 pid=8386 rate_limit=100 backlog_limit=320 lost=10534 backlog=0
+STATUS=($(/sbin/auditctl -s))
 
-if [[ ${TOK[1]} != 'enabled=1' ]]; then
-    echo "audit has been disabled."
-    EX=2
-fi
-
-if [[ ${TOK[3]} == 'pid=0' ]]; then
-    echo "auditd process died or non-responsive."
-    EX=2
+if [[ ${STATUS[@]:0:1} == 'AUDIT_STATUS:' ]]; then
+    if [[ ${STATUS[@]:1:1} != 'enabled=1' ]]; then
+        echo "audit has been disabled."
+        EX=$(($EX+2))
+    fi
+else
+    if [[ ${STATUS[@]:0:1} != 'enabled' && ${STATUS[@]:1:1} != '1' ]]; then
+        echo "audit has been disabled."
+        EX=$(($EX+2))
+    fi
+    if [[ ${STATUS[@]:4:1} == 'pid' && ${STATUS[@]:5:1} == '0' ]]; then
+        echo "auditd process died or non-responsive."
+        EX=$(($EX+3))
+    fi
 fi
 
 # audisp-json is installed, so we expect it to be running, vs audisp-cef
 if [[ ! $(pidof audisp-json) ]]; then
     echo "audisp-json process not present, but should be."
-    EX=2
+    EX=$(($EX+5))
 fi
 
 if [[ $EX -eq 0 ]]; then
@@ -28,3 +44,4 @@ if [[ $EX -eq 0 ]]; then
 fi
 
 exit $EX
+
